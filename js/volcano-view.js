ca19a77ef6dda53d734694d8730ba3e07e2edaf0
ofr1tz <ofr1tz@yahoo.de>
2021-06-18 16:36:52 +0100

    img bg

--- js/volcano-view.js
+++ js/volcano-view.js
@@ -204,7 +204,8 @@
 				var coords = f.getGeometry().getCoordinates();
 				var name = f.get('name');
 				var ele = f.get('ele');				
-				popup.show(coords, '<div><h2>'+name+' ('+ele+'m)</h2><p>Click to show/hide viewshed.</p><img src="img/'+name+'.png" alt="'+name+'" width="125px" height="125px"></div>');
+				// popup.show(coords, '<div><h2>'+name+' ('+ele+'m)</h2><p>Click to show/hide viewshed.</p><img src="img/'+name+'.png" alt="'+name+'" width="125px" height="125px"></div>');
+				popup.show(coords, '<div style="background: url"img/'+name+'.png") no-repeat center center"><h2>'+name+' ('+ele+'m)</h2><p>Click to show/hide viewshed.</p><img src="img/'+name+'.png" alt="'+name+'" width="125px" height="125px"></div>');
 			});
 		});	
 
